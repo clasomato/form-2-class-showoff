97f068f8d8a601341b00fb5f3b7e0052338620bb
GabrielArcus <gabriel.billingarcus@gmail.com>
2020-08-27 11:40:23 +1200

    adding css and refresh

--- showoff/src/main.js
+++ showoff/src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+import 'bootstrap'
+import 'fontawesome'
+import '@fortawesome/fontawesome-free/css/all.css'
+import '@fortawesome/fontawesome-free/js/all.js'
 import router from './router'
 import store from './store'
-
 import 'toastr/build/toastr.min.css'
 
 Vue.config.productionTip = false
